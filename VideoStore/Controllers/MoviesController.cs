3d59b94bc04bd6fdb395c82cb32446795c4190e8
Justin Scroggins <justinscroggins@outlook.com>
2021-10-19 17:32:26 -0400

    Updated mavbar and added mdbootstrap

--- VideoStore/Controllers/MoviesController.cs
+++ VideoStore/Controllers/MoviesController.cs
@@ -10,10 +10,35 @@ namespace VideoStore.Controllers
     public class MoviesController : Controller
     {
         // GET: Movies/Random
-        public ActionResult Random()
+        public ViewResult Random()
         {
             var Movie = new Movie() { Id = 1, Name = "Shrek" };
             return View(Movie);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+        // Movies
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+            {
+                pageIndex = 1;
+            }
+
+            if (String.IsNullOrWhiteSpace(sortBy))
+            {
+                sortBy = "Name";
+            }
+
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
+
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
         }
     }
 }
