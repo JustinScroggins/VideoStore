f2156e608747fe90164f34d6aaf535092b3a2188
Justin Scroggins <justinscroggins@outlook.com>
2021-10-19 09:53:51 -0400

    Add project files.

---
+++ VideoStore/Controllers/MoviesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VideoStore.Models;
+
+namespace VideoStore.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var Movie = new Movie() { Id = 1, Name = "Shrek" };
+            return View(Movie);
+        }
+    }
+}
